da2866bb4db6a585052511b9e7f312f2c1b92ef0
Dang Huu Thinh <thinh10021121@gmail.com>
2024-07-13 20:25:40 +0700

    Create first problem

--- src/main/java/com/danghuuthinh/Main.java
+++ src/main/java/com/danghuuthinh/Main.java
@@ -2,6 +2,10 @@ package com.danghuuthinh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int x =10;
+        int y = (5 + x) * x - x;
+
+        System.out.println(y);
     }
 }
